cef28f31fe69ebba33432616e0da9182241df374
Mani kiran <m.manikiran@yahoo.com>
2018-05-20 11:27:45 +0000

    candy

---
+++ ScannerSample.java
@@ -0,0 +1,34 @@
+import java.lang.*;
+import java.io.*;
+import java.util.*;
+class ScannerSample{
+
+	public static void main(String[] args){
+		
+		int no_of_elephants = 0;
+		int no_of_candies   = 0;
+		Scanner sc = new Scanner(System.in);
+		int t = sc.hasNextLine() ? sc.nextInt() : 0 ;
+		String[] outputs = new String[t];
+
+		while(t-- > 0){
+
+			no_of_elephants = sc.nextInt();
+			no_of_candies   = sc.nextInt();
+
+			for(int i = 0; i < no_of_elephants && 
+						no_of_candies >= 0; i++){
+				no_of_candies -= sc.nextInt();
+				
+			}
+			String s = sc.nextLine();
+			outputs[t] = no_of_candies < 0 ? "No" : "Yes";
+			
+
+		}
+
+			for(int i = (outputs.length)-1; i >= 0; i--)
+				System.out.println(outputs[i]);
+	}
+	
+}
